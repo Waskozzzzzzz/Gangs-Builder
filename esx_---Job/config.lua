4980168599b824ec174475ad964703ee55f805dc
Waskozzzzzzz <94469783+Waskozzzzzzz@users.noreply.github.com>
2022-06-15 18:20:59 +0300

    Add files via upload

---
+++ esx_---Job/config.lua
@@ -0,0 +1,146 @@
+Config                            = {}
+
+Config.DrawDistance               = 100.0
+Config.MarkerType                 = 1
+Config.MarkerSize                 = { x = 1.5, y = 1.5, z = 0.5 }
+Config.MarkerColor                = { r = 50, g = 50, b = 204 }
+
+Config.EnablePlayerManagement     = true
+Config.EnableArmoryManagement     = true
+Config.EnableESXIdentity          = true -- enable if you're using esx_identity
+Config.EnableNonFreemodePeds      = false -- turn this on if you want custom peds
+Config.EnableLicenses             = true -- enable if you're using esx_license
+
+Config.EnableHandcuffTimer        = false -- enable handcuff timer? will unrestrain player after the time ends
+Config.HandcuffTimer              = 10 * 60000 -- 10 mins
+
+Config.EnableJobBlip              = false -- enable blips for colleagues, requires esx_society
+
+Config.MaxInService               = -1
+Config.Locale = 'de'
+
+Config.PoliceStations = {
+
+	--- = {
+
+		Cloakrooms = {
+		
+		},
+
+		Armories = {
+			vector3(-18.44,-1438.8,31.1)
+		},
+
+		Vehicles = {
+			{
+				Spawner = vector3(-19.54,-1430.49,30.9),
+				InsideShop = vector3(-24.25,-1436.86,30.65),
+				SpawnPoints = {
+					{ coords = vector3(-24.25,-1436.86,30.65), heading = 179.13, radius = 6.0 }
+				}
+			}
+		},
+		
+
+		BossActions = {
+			vector3(-17.77,-1432.11,31.1)
+		}
+
+	}
+
+}
+
+Config.AuthorizedWeapons = {
+	recruit = {},
+
+	officer = {},
+
+	sergeant = {},
+
+	intendent = {},
+
+	lieutenant = {},
+
+	chef = {},
+
+	boss = {
+		{ weapon = 'WEAPON_PISTOL', price = 0 },
+		{ weapon = 'WEAPON_FLASHLIGHT', price = 0 }
+	}
+}
+
+Config.AuthorizedVehicles = {
+	Shared = {
+		{ model = 'sultanrs', label = 'Sultan', price = 1 }
+	},
+
+	recruit = {
+		
+
+	},
+
+	officer = {
+		
+	},
+
+	sergeant = {
+		
+
+	},
+
+	intendent = {},
+
+	lieutenant = {},
+
+	chef = {},
+
+	boss = {
+		
+
+	}
+}
+
+Config.AuthorizedHelicopters = {
+	recruit = {},
+
+	officer = {},
+
+	sergeant = {},
+
+	intendent = {},
+
+	lieutenant = {},
+
+	chef = {},
+
+	boss = {}
+}
+
+-- CHECK SKINCHANGER CLIENT MAIN.LUA for matching elements
+
+Config.Uniforms = {
+	recruit_wear = {},
+	officer_wear = {},
+	sergeant_wear = {},
+	intendent_wear = {},
+	lieutenant_wear = {},
+	chef_wear = {},
+	boss_wear = { },
+	bullet_wear = {
+		male = {
+			['bproof_1'] = 27,  ['bproof_2'] = 5
+		},
+		female = {
+			['bproof_1'] = 29,  ['bproof_2'] = 5
+		}
+	},
+	gilet_wear = {
+		male = {
+			['tshirt_1'] = 59,  ['tshirt_2'] = 1
+		},
+		female = {
+			['tshirt_1'] = 36,  ['tshirt_2'] = 1
+		}
+	}
+
+}
---
+++ esx_---Job/server/main.lua
@@ -0,0 +1,454 @@
+fESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+if Config.MaxInService ~= -1 then
+  TriggerEvent('esx_service:activateService', '---', Config.MaxInService)
+end
+
+-- TriggerEvent('esx_phone:registerNumber', '---', _U('alert_---'), true, true)
+TriggerEvent('esx_society:registerSociety', '---', '---', 'society_---', 'society_---', 'society_---', {type = 'public'})
+
+RegisterServerEvent('esx_---job:giveWeapon')
+AddEventHandler('esx_---job:giveWeapon', function(weapon, ammo)
+  local xPlayer = ESX.GetPlayerFromId(source)
+  xPlayer.addWeapon(weapon, ammo)
+end)
+
+RegisterServerEvent('esx_---job:confiscatePlayerItem')
+AddEventHandler('esx_---job:confiscatePlayerItem', function(target, itemType, itemName, amount)
+
+  local sourceXPlayer = ESX.GetPlayerFromId(source)
+  local targetXPlayer = ESX.GetPlayerFromId(target)
+
+  if itemType == 'item_standard' then
+
+    local label = sourceXPlayer.getInventoryItem(itemName).label
+
+    targetXPlayer.removeInventoryItem(itemName, amount)
+    sourceXPlayer.addInventoryItem(itemName, amount)
+
+    TriggerClientEvent('esx:showNotification', sourceXPlayer.source, _U('you_have_confinv') .. amount .. ' ' .. label .. _U('from') .. targetXPlayer.name)
+    TriggerClientEvent('esx:showNotification', targetXPlayer.source, '~b~' .. targetXPlayer.name .. _U('confinv') .. amount .. ' ' .. label )
+
+  end
+
+  if itemType == 'item_account' then
+
+    targetXPlayer.removeAccountMoney(itemName, amount)
+    sourceXPlayer.addAccountMoney(itemName, amount)
+
+    TriggerClientEvent('esx:showNotification', sourceXPlayer.source, _U('you_have_confdm') .. amount .. _U('from') .. targetXPlayer.name)
+    TriggerClientEvent('esx:showNotification', targetXPlayer.source, '~b~' .. targetXPlayer.name .. _U('confdm') .. amount)
+
+  end
+
+  if itemType == 'item_weapon' then
+
+    targetXPlayer.removeWeapon(itemName)
+    sourceXPlayer.addWeapon(itemName, amount)
+
+    TriggerClientEvent('esx:showNotification', sourceXPlayer.source, _U('you_have_confweapon') .. ESX.GetWeaponLabel(itemName) .. _U('from') .. targetXPlayer.name)
+    TriggerClientEvent('esx:showNotification', targetXPlayer.source, '~b~' .. targetXPlayer.name .. _U('confweapon') .. ESX.GetWeaponLabel(itemName))
+
+  end
+
+end)
+
+RegisterServerEvent('esx_---job:handcuff')
+AddEventHandler('esx_---job:handcuff', function(target)
+  TriggerClientEvent('esx_---job:handcuff', target)
+end)
+
+RegisterServerEvent('esx_---job:drag')
+AddEventHandler('esx_---job:drag', function(target)
+  local _source = source
+  TriggerClientEvent('esx_---job:drag', target, _source)
+end)
+
+RegisterServerEvent('esx_---job:putInVehicle')
+AddEventHandler('esx_---job:putInVehicle', function(target)
+  TriggerClientEvent('esx_---job:putInVehicle', target)
+end)
+
+RegisterServerEvent('esx_---job:OutVehicle')
+AddEventHandler('esx_---job:OutVehicle', function(target)
+    TriggerClientEvent('esx_---job:OutVehicle', target)
+end)
+
+RegisterServerEvent('esx_---job:getStockItem')
+AddEventHandler('esx_---job:getStockItem', function(itemName, count)
+
+  local xPlayer = ESX.GetPlayerFromId(source)
+
+  TriggerEvent('esx_addoninventory:getSharedInventory', 'society_---', function(inventory)
+
+    local item = inventory.getItem(itemName)
+
+    if item.count >= count then
+      inventory.removeItem(itemName, count)
+      xPlayer.addInventoryItem(itemName, count)
+    else
+      TriggerClientEvent('esx:showNotification', xPlayer.source, _U('quantity_invalid'))
+    end
+
+    TriggerClientEvent('esx:showNotification', xPlayer.source, _U('have_withdrawn') .. count .. ' ' .. item.label)
+
+  end)
+
+end)
+
+RegisterServerEvent('esx_---job:putStockItems')
+AddEventHandler('esx_---job:putStockItems', function(itemName, count)
+
+  local xPlayer = ESX.GetPlayerFromId(source)
+
+  TriggerEvent('esx_addoninventory:getSharedInventory', 'society_---', function(inventory)
+
+    local item = inventory.getItem(itemName)
+
+    if item.count >= 0 then
+      xPlayer.removeInventoryItem(itemName, count)
+      inventory.addItem(itemName, count)
+    else
+      TriggerClientEvent('esx:showNotification', xPlayer.source, _U('quantity_invalid'))
+    end
+
+    TriggerClientEvent('esx:showNotification', xPlayer.source, _U('added') .. count .. ' ' .. item.label)
+
+  end)
+
+end)
+
+ESX.RegisterServerCallback('esx_---job:getOtherPlayerData', function(source, cb, target)
+
+  if Config.EnableESXIdentity then
+
+    local xPlayer = ESX.GetPlayerFromId(target)
+
+    local identifier = GetPlayerIdentifiers(target)[1]
+
+    local result = MySQL.Sync.fetchAll("SELECT * FROM users WHERE identifier = @identifier", {
+      ['@identifier'] = identifier
+    })
+
+    local user      = result[1]
+    local firstname     = user['firstname']
+    local lastname      = user['lastname']
+    local sex           = user['sex']
+    local dob           = user['dateofbirth']
+    local height        = user['height'] .. " Inches"
+
+    local data = {
+      name        = GetPlayerName(target),
+      job         = xPlayer.job,
+      inventory   = xPlayer.inventory,
+      accounts    = xPlayer.accounts,
+      weapons     = xPlayer.loadout,
+      firstname   = firstname,
+      lastname    = lastname,
+      sex         = sex,
+      dob         = dob,
+      height      = height
+    }
+
+    TriggerEvent('esx_status:getStatus', source, 'drunk', function(status)
+
+      if status ~= nil then
+        data.drunk = math.floor(status.percent)
+      end
+
+    end)
+
+    if Config.EnableLicenses then
+
+      TriggerEvent('esx_license:getLicenses', source, function(licenses)
+        data.licenses = licenses
+        cb(data)
+      end)
+
+    else
+      cb(data)
+    end
+
+  else
+
+    local xPlayer = ESX.GetPlayerFromId(target)
+
+    local data = {
+      name       = GetPlayerName(target),
+      job        = xPlayer.job,
+      inventory  = xPlayer.inventory,
+      accounts   = xPlayer.accounts,
+      weapons    = xPlayer.loadout
+    }
+    TriggerEvent('', _source, '', function(status)
+
+      if status ~= nil then
+        data.drunk = status.getPercent()
+      end
+
+    end)
+
+    TriggerEvent('', _source, function(licenses)
+      data.licenses = licenses
+    end)
+
+    cb(data)
+
+  end
+
+end)
+
+ESX.RegisterServerCallback('esx_---job:getFineList', function(source, cb, category)
+
+  MySQL.Async.fetchAll(
+    'SELECT * FROM fine_types_--- WHERE category = @category',
+    {
+      ['@category'] = category
+    },
+    function(fines)
+      cb(fines)
+    end
+  )
+
+end)
+
+ESX.RegisterServerCallback('esx_---job:getVehicleInfos', function(source, cb, plate)
+
+  if Config.EnableESXIdentity then
+
+    MySQL.Async.fetchAll(
+      'SELECT * FROM owned_vehicles',
+      {},
+      function(result)
+
+        local foundIdentifier = nil
+
+        for i=1, #result, 1 do
+
+          local vehicleData = json.decode(result[i].vehicle)
+
+          if vehicleData.plate == plate then
+            foundIdentifier = result[i].owner
+            break
+          end
+
+        end
+
+        if foundIdentifier ~= nil then
+
+          MySQL.Async.fetchAll(
+            'SELECT * FROM users WHERE identifier = @identifier',
+            {
+              ['@identifier'] = foundIdentifier
+            },
+            function(result)
+
+              local ownerName = result[1].firstname .. " " .. result[1].lastname
+
+              local infos = {
+                plate = plate,
+                owner = ownerName
+              }
+
+              cb(infos)
+
+            end
+          )
+
+        else
+
+          local infos = {
+          plate = plate
+          }
+
+          cb(infos)
+
+        end
+
+      end
+    )
+
+  else
+
+    MySQL.Async.fetchAll(
+      'SELECT * FROM owned_vehicles',
+      {},
+      function(result)
+
+        local foundIdentifier = nil
+
+        for i=1, #result, 1 do
+
+          local vehicleData = json.decode(result[i].vehicle)
+
+          if vehicleData.plate == plate then
+            foundIdentifier = result[i].owner
+            break
+          end
+
+        end
+
+        if foundIdentifier ~= nil then
+
+          MySQL.Async.fetchAll(
+            'SELECT * FROM users WHERE identifier = @identifier',
+            {
+              ['@identifier'] = foundIdentifier
+            },
+            function(result)
+
+              local infos = {
+                plate = plate,
+                owner = result[1].name
+              }
+
+              cb(infos)
+
+            end
+          )
+
+        else
+
+          local infos = {
+          plate = plate
+          }
+
+          cb(infos)
+
+        end
+
+      end
+    )
+
+  end
+
+end)
+
+ESX.RegisterServerCallback('esx_---job:getArmoryWeapons', function(source, cb)
+
+  TriggerEvent('esx_datastore:getSharedDataStore', 'society_---', function(store)
+
+    local weapons = store.get('weapons')
+
+    if weapons == nil then
+      weapons = {}
+    end
+
+    cb(weapons)
+
+  end)
+
+end)
+
+ESX.RegisterServerCallback('esx_---job:addArmoryWeapon', function(source, cb, weaponName)
+
+  local xPlayer = ESX.GetPlayerFromId(source)
+
+  xPlayer.removeWeapon(weaponName)
+
+  TriggerEvent('esx_datastore:getSharedDataStore', 'society_---', function(store)
+
+    local weapons = store.get('weapons')
+
+    if weapons == nil then
+      weapons = {}
+    end
+
+    local foundWeapon = false
+
+    for i=1, #weapons, 1 do
+      if weapons[i].name == weaponName then
+        weapons[i].count = weapons[i].count + 1
+        foundWeapon = true
+      end
+    end
+
+    if not foundWeapon then
+      table.insert(weapons, {
+        name  = weaponName,
+        count = 1
+      })
+    end
+
+     store.set('weapons', weapons)
+
+     cb()
+
+  end)
+
+end)
+
+ESX.RegisterServerCallback('esx_---job:removeArmoryWeapon', function(source, cb, weaponName)
+
+  local xPlayer = ESX.GetPlayerFromId(source)
+
+  xPlayer.addWeapon(weaponName, 1000)
+
+  TriggerEvent('esx_datastore:getSharedDataStore', 'society_---', function(store)
+
+    local weapons = store.get('weapons')
+
+    if weapons == nil then
+      weapons = {}
+    end
+
+    local foundWeapon = false
+
+    for i=1, #weapons, 1 do
+      if weapons[i].name == weaponName then
+        weapons[i].count = (weapons[i].count > 0 and weapons[i].count - 1 or 0)
+        foundWeapon = true
+      end
+    end
+
+    if not foundWeapon then
+      table.insert(weapons, {
+        name  = weaponName,
+        count = 0
+      })
+    end
+
+     store.set('weapons', weapons)
+
+     cb()
+
+  end)
+
+end)
+
+
+ESX.RegisterServerCallback('esx_---job:buy', function(source, cb, amount)
+
+  TriggerEvent('esx_addonaccount:getSharedAccount', 'society_---', function(account)
+
+    if account.money >= amount then
+      account.removeMoney(amount)
+      cb(true)
+    else
+      cb(false)
+    end
+
+  end)
+
+end)
+
+ESX.RegisterServerCallback('esx_---job:getStockItems', function(source, cb)
+
+  TriggerEvent('esx_addoninventory:getSharedInventory', 'society_---', function(inventory)
+    cb(inventory.items)
+  end)
+
+end)
+
+ESX.RegisterServerCallback('esx_---job:getPlayerInventory', function(source, cb)
+
+  local xPlayer = ESX.GetPlayerFromId(source)
+  local items   = xPlayer.inventory
+
+  cb({
+    items = items
+  })
+
+end)
